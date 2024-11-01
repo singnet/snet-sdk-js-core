5837bf936ac77a018d85322007878a1dac877db7
Marina Fedyantceva <marina.fedyantzeva@singularitynet.io>
2024-11-01 17:17:44 +0300

    [SPT-679] moved index file

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import SnetSDK from './sdk';
+
+export default SnetSDK;
+
+// export { default as logger } from './utils/logger';
+export { default as EncodingUtils } from './utils/encodingUtils';
+export { default as blockChainEvents } from './utils/blockchainEvents';
+export { default as BaseServiceClient } from './BaseServiceClient';
+export { default as PaymentChannel } from './PaymentChannel';
+export * from './identities';
