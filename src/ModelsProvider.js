be3e738d203d294600d739074e55e6290682691e
Artash <artashes@vbrl.ai>
2025-01-15 10:42:39 +0300

    [SPT-726] Refactor ModelsProvider.js logs

--- src/ModelsProvider.js
+++ src/ModelsProvider.js
@@ -1,51 +1,37 @@
+import { logMessage } from "./utils/logger";
+
 export class ChannelModelProvider {
     generatePaymentChannelStateServiceClient() {
-        error(
-            '_generatePaymentChannelStateServiceClient must be implemented in the sub classes'
-        );
+        logMessage('error', 'ChannelModelProvider', '_generatePaymentChannelStateServiceClient must be implemented in the sub classes');
     }
 
     getChannelStateRequestMethodDescriptor() {
-        error(
-            'getChannelStateRequestMethodDescriptor must be implemented in the sub classes'
-        );
+        logMessage('error', 'ChannelModelProvider', '_getChannelStateRequestMethodDescriptor must be implemented in the sub classes');
     }
 }
 
 export class TrainingModelProvider {
     getModelRequestMethodDescriptor() {
-        error(
-            '_getModelRequestMethodDescriptor must be implemented in the sub classes'
-        );
+        logMessage('error', 'TrainingModelProvider', '_getModelRequestMethodDescriptor must be implemented in the sub classes');
     }
 
     getAuthorizationRequestMethodDescriptor() {
-        error(
-            '_getAuthorizationRequestMethodDescriptor must be implemented in the sub classes'
-        );
+        logMessage('error', 'TrainingModelProvider', '_getAuthorizationRequestMethodDescriptor must be implemented in the sub classes');
     }
 
     getCreateModelRequestMethodDescriptor() {
-        error(
-            '_getCreateModelRequestMethodDescriptor must be implemented in the sub classes'
-        );
+        logMessage('error', 'TrainingModelProvider', '_getCreateModelRequestMethodDescriptor must be implemented in the sub classes');
     }
 
     getDeleteModelRequestMethodDescriptor() {
-        error(
-            '_getDeleteModelRequestMethodDescriptor must be implemented in the sub classes'
-        );
+        logMessage('error', 'TrainingModelProvider', '_getDeleteModelRequestMethodDescriptor must be implemented in the sub classes');
     }
 
     getUpdateModelRequestMethodDescriptor() {
-        error(
-            '__getUpdateModelRequestMethodDescriptor must be implemented in the sub classes'
-        );
+        logMessage('error', 'TrainingModelProvider', '__getUpdateModelRequestMethodDescriptor must be implemented in the sub classes');
     }
 
     getModelDetailsRequestMethodDescriptor() {
-        error(
-            '_getModelDetailsRequestMethodDescriptor must be implemented in the sub classes'
-        );
+        logMessage('error', 'TrainingModelProvider', '_getModelDetailsRequestMethodDescriptor must be implemented in the sub classes');
     }
 }
