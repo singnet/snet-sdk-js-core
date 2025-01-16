5978e884abb772a99a45f8d90bcbcd638dcdd1c7
Artash <artashes@vbrl.ai>
2025-01-16 11:06:52 +0300

    [SPT-726] Add util function to log objects as string

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -6,6 +6,26 @@ const formatLogMessage = (level, moduleName, message) => {
 };
 
 /**
+ * Converts objects or arrays of objects into a readable string,
+ * replacing BigInt values with strings.
+ *
+ * @param {Object|Array} data - The object(s) to stringify.
+ * @returns {string} - A formatted string with BigInt values replaced.
+ */
+export const stringifyWithBigInt = (data) => {
+    const replacer = (_, value) => {
+        return typeof value === 'bigint' ? value.toString() : value; // Convert BigInt to string
+    };
+
+    if (Array.isArray(data)) {
+        return data.map((item) => JSON.stringify(item, replacer, 2)).join(', ');
+    }
+
+    return JSON.stringify(data, replacer, 2);
+};
+
+
+/**
  * Logs a message in the specified format.
  * @param {string} level - The log level ('debug', 'info', 'error').
  * @param {string} moduleName - The name of the module.
