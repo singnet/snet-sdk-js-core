6ea1e7f51e6f4c774c3da9cf6a392ad5211aea9e
Marina Fedyantseva <marie.saez@mail.ru>
2025-05-12 17:49:21 +0300

    [SJS-2] add validation of config

--- src/sdk.js
+++ src/sdk.js
@@ -7,15 +7,7 @@ import IPFSMetadataProvider from './IPFSMetadataProvider';
 import { DefaultPaymentStrategy } from './payment_strategies';
 import { setLevel } from 'loglevel';
 import { logMessage } from './utils/logger';
-
-const DEFAULT_CONFIG = {
-    defaultGasLimit: 210000,
-    defaultGasPrice: 4700000,
-    ipfsEndpoint: 'https://ipfs.singularitynet.io:443',
-    logLevel: 'info',
-    tokenName: 'FET',
-    standType: 'prod'
-};
+import { DEFAULT_CONFIG, validateConfig } from './utils/configHelper';
 
 class SnetSDK {
     /**
@@ -23,6 +15,7 @@ class SnetSDK {
      * @param {IPFSMetadataProvider} metadataProvider
      */
     constructor(config, metadataProvider = undefined) {
+        validateConfig(config);
         this._config = {
             ...DEFAULT_CONFIG,
             ...config,
---
+++ src/utils/configHelper.js
@@ -0,0 +1,80 @@
+import { VALID_LOG_LEVELS } from "./logger";
+import { TOKEN_NAMES } from "./tokenUtils";
+
+export const DEFAULT_CONFIG = {
+    defaultGasLimit: 210000,
+    defaultGasPrice: 4700000,
+    ipfsEndpoint: 'https://ipfs.singularitynet.io:443',
+    logLevel: 'info',
+    tokenName: 'FET',
+    standType: 'prod'
+};
+
+const NETWORK_ID = {
+     MAINNET: 1,
+     SEPOLIA: 11155111  
+}
+
+const STAND_TYPES = {
+    PRODUCTION: 'prod',
+    DEMO: 'demo',
+    DEVELOPMENT: 'dev'  
+}
+
+const validateStandType = (standType, networkId) => {
+    if (!standType) {
+        return;
+    }
+
+    if (networkId === NETWORK_ID.MAINNET && standType !== STAND_TYPES.PRODUCTION) {
+        throw new Error(`Mainnet only supports stand type: ${STAND_TYPES.PRODUCTION}`)
+    }
+
+    if (networkId === NETWORK_ID.SEPOLIA && standType !== STAND_TYPES.DEMO && standType !== STAND_TYPES.DEVELOPMENT) {
+        throw new Error(`Sepolia supports stand types: ${STAND_TYPES.DEMO} or ${STAND_TYPES.DEVELOPMENT}`)
+    }
+}
+
+const isValidUrl = (urlString) => {
+    try {
+      const url = new URL(urlString);
+      return url.protocol === 'http:' || url.protocol === 'https:';
+    } catch (err) {
+      return false;
+    }
+  };
+
+export const validateConfig = (config) => {
+    if (!config || typeof config !== 'object') {
+        throw new Error('Configuration must be an object');
+    }
+
+    const validTokenNames = Object.keys(TOKEN_NAMES);
+    if (!validTokenNames.includes(config.tokenName)) {
+        throw new Error(`Token must be one of: ${validTokenNames.join(", ")}`)
+    }
+
+    if (!config?.web3Provider) {
+        throw new Error('Web3 provider is required');
+    }
+
+    if (typeof config.networkId !== 'number') {
+        throw new Error('Network ID must be a number');
+    }
+
+    const validNetworkIds = Object.values(NETWORK_ID);
+    if (!validNetworkIds.includes(config.networkId)) {
+        throw new Error(`Network ID must be one of: ${validNetworkIds.join(', ')}`);
+    }
+
+    validateStandType(config.standType, config.networkId);
+
+    const validLogLevels = Object.keys(VALID_LOG_LEVELS);
+    if (config.logLevel && !validLogLevels.includes(config.logLevel)) {
+        throw new Error(`Log level must be one of: ${validLogLevels.join(', ')}`);
+    }
+
+    if (config.ipfsEndpoint && !isValidUrl(config.ipfsEndpoint)) {
+        throw new Error('IPFS endpoint must be a valid HTTP/HTTPS URL');
+    }
+}
